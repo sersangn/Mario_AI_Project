c3454ed77e861c1d2259dd071386d05a3db0e50c
sersang <sersang01@gmail.com>
2023-10-12 21:29:41 +0800

    Working on rule based

---
+++ Reinforcement_learning/reinforcement_learning.py
@@ -0,0 +1,85 @@
+# Import the game
+import gym_super_mario_bros
+# Import the Joypad wrapper
+from nes_py.wrappers import JoypadSpace
+# Import the SIMPLIFIED controls
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+# Import Frame Stacker Wrapper and GrayScaling Wrapper
+from gym.wrappers import GrayScaleObservation #diff
+# Import Vectorization Wrappers
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+# Import Matplotlib to show the  impact of frame stacking
+from matplotlib import pyplot as plt
+# Import os for file path management
+import os 
+# Import PPO for algos
+from stable_baselines3 import PPO
+# Import Base Callback for saving models
+from stable_baselines3.common.callbacks import BaseCallback
+
+#JoypadSpace.reset = lambda self, **kwargs: self.env.reset(**kwargs)
+
+#Preprocessing the environment
+# 1. Create the base environment
+env = gym_super_mario_bros.make('SuperMarioBros-v0',)
+
+# 2. Simplify the controls 
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+# 3. Grayscale
+env = GrayScaleObservation(env, keep_dim=True)
+
+# 4. Wrap inside the Dummy Environment
+# env = DummyVecEnv([lambda: env])
+# 5. Stack the frames
+# env = VecFrameStack(env, 4, channels_order='last')
+state = env.reset()
+print(state.shape)
+
+# #Train the RL model
+# class TrainAndLoggingCallback(BaseCallback):
+
+#     def __init__(self, check_freq, save_path, verbose=1):
+#         super(TrainAndLoggingCallback, self).__init__(verbose)
+#         self.check_freq = check_freq
+#         self.save_path = save_path
+
+#     def _init_callback(self):
+#         if self.save_path is not None:
+#             os.makedirs(self.save_path, exist_ok=True)
+
+#     def _on_step(self):
+#         if self.n_calls % self.check_freq == 0:
+#             model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_calls))
+#             self.model.save(model_path)
+
+#         return True
+    
+# CHECKPOINT_DIR = './train/'
+# LOG_DIR = './logs/'
+
+# # Setup model saving callback
+# callback = TrainAndLoggingCallback(check_freq=10000, save_path=CHECKPOINT_DIR)
+
+# # This is the AI model started
+# model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=LOG_DIR, learning_rate=0.000001, 
+#             n_steps=512) 
+
+# # Train the AI model, this is where the AI model starts to learn
+# model.learn(total_timesteps=1000000, callback=callback)
+
+# model.save('thisisatestmodel')
+
+
+# Load model
+# model = PPO.load('./train/best_model_1000000')
+
+# state = env.reset()
+
+# # Start the game 
+# state = env.reset()
+# # Loop through the game
+# while True: 
+    
+#     action, _ = model.predict(state)
+#     state, reward, done, info = env.step(action)
+#     env.render()
