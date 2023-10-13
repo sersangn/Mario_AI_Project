aa30abacd059ec81bf1c4bc076c17de5ecb6721d
sersang <sersang01@gmail.com>
2023-10-13 23:04:50 +0800

    Cleaned up code and changed enemy conditions

--- mario_locate_objects2.py
+++ mario_locate_objects2.py
@@ -357,9 +357,21 @@ def make_action(screen, info, step, env, prev_action,gap):
     #              action = 2 means press 'right' and 'A' buttons at the same time
     #check if you're in the middle of a manuever - add something to keep track of 
     #step variable -- tells you what time, step+10
-
-        # I have no strategy at the moment, so I'll choose a random action.
+        
+    #DEFAULT ACTION
     action = 3
+    #-------------------ENEMY LOCATION CODE-------------------#
+
+    ##Code for jumping according to enemy locations
+    if len(enemy_locations)>0:
+        for enemy in enemy_locations:
+            if enemy[0][0]>mario_locations[0][0][0]:
+                if(enemy[0][0]-mario_locations[0][0][0]<50) and (mario_locations[0][0][0]<enemy[0][0]) and (enemy[0][1]>=193):
+                    print(enemy)
+                    print(mario_locations)
+                    action=4
+
+    #-------------------PIPE CODE-------------------#
     pipeindex = 0
     pipelist = []
     pipe = False
@@ -367,14 +379,8 @@ def make_action(screen, info, step, env, prev_action,gap):
             if 'pipe' in element:
                 pipe = True
                 pipelist.append(index)
-                
-    if len(enemy_locations)>0:
-        for enemy in enemy_locations:
-            if enemy[0][0]>mario_locations[0][0][0]:
-                if(enemy[0][0]-mario_locations[0][0][0]<50) and (mario_locations[0][0][0]<enemy[0][0]) and (enemy[0][1]<=193) :
-                    print(enemy)
-                    print(mario_locations)
-                    action=4
+
+    #Code for jumping with pipe
     if pipe:
         for pipeindex in pipelist:
             # print("pipe", pipeindex, block_locations[pipeindex]) 
@@ -388,18 +394,29 @@ def make_action(screen, info, step, env, prev_action,gap):
             botlevel.append(block)
     #got some exception error but program continued... might introduce errors later
 
+    #-------------------DEALING WITH GAPS CODE-------------------#
 
-    ###GAP CODE
     if len(botlevel) <= 13:
         prevblock = botlevel[0]
         for block in botlevel:
             if ((block[0][0] - prevblock[0][0]) !=0 )and ((block[0][0] - prevblock[0][0]!=16)):
-                if prevblock[0][0]+16 <= mario_locations[0][0][0] or mario_locations[0][0][0] <= block[0][0]:
-                    print("HERE")
+                if prevblock[0][0]+32 <= mario_locations[0][0][0] or mario_locations[0][0][0] <= block[0][0]:
+                    print("GAP HERE")
                     action= 4
             prevblock= block
-        #Stops mario from jumping twice 
+        
+    #Trying to deal with four goombas
+    # if len(enemy_locations)==3:
+    #     for enemy in enemy_locations:
+    #         if enemy[0][0]>mario_locations[0][0][0]:
+    #             if(enemy[0][0]-mario_locations[0][0][0]<5) and (mario_locations[0][0][0]<enemy[0][0]) and (enemy[0][1]<=193):
+    #                 print("HEREEEEE")
+    #                 action = 6
+    #                 return action
 
+
+######Stops mario from always pressing jump and getting stuck
+#--------Every 10 frames you switch action to 0
     if step % 10 == 0:
         if prev_action == 4:
             action = 0
@@ -413,8 +430,9 @@ def make_action(screen, info, step, env, prev_action,gap):
     #         print(floor)
     # print("MARIO", mario_locations)
     # print("FLOOR", botlevel)
-    #######NEW GAP METHOD#####
 
+    ##-----------------------NEW GAP METHOD------------------------#
+    ##--------DEALING WITH THIS USING THE LENGTH _ NOT THAT GREAT
     # #####OLD GAP METHOD
     # if len(floor) <= 26:
     #     gap=gap+1
